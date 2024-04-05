a5a92609397c7c5237f8888d149e4572f6bfdd72
bernardo-mcosta <76459310+bernardo-mcosta@users.noreply.github.com>
2024-04-05 20:15:01 +0000

    add terraform configuration files

---
+++ 01-terraform/variables.tf
@@ -0,0 +1,25 @@
+locals {
+  data_lake_bucket = "electric_vehicles"
+}
+
+variable "project" {
+  description = "Your GCP Project ID"
+  # default = "Your GCP Project ID"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default     = "southamerica-east1"
+  type        = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default     = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type        = string
+  default     = "electric-vehicles"
+}
