1713df9ba14b1f88f96309a3ee2c751bc3ff6092
bernardo-mcosta <76459310+bernardo-mcosta@users.noreply.github.com>
2024-04-12 19:09:37 +0000

    Correction for SQL data exporter

--- 02-mage/electric_vehicles/data_exporters/vehicles_to_bigquery.sql
+++ 02-mage/electric_vehicles/data_exporters/vehicles_to_bigquery.sql
@@ -1,3 +1,4 @@
+-- Docs: https://docs.mage.ai/guides/sql-blocks
 CREATE OR REPLACE EXTERNAL TABLE `dtc-de-course-412223.electric_vehicles.electric_vehicles_data`
 WITH PARTITION COLUMNS (transaction_year INT64)
 OPTIONS (
