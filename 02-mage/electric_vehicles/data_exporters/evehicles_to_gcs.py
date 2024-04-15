13f27d719ced393324c6d30e0ad4dd069a6b4a8f
Bernardo <76459310+bernardo-mcosta@users.noreply.github.com>
2024-04-15 13:41:20 -0300

    remove unused code

--- 02-mage/electric_vehicles/data_exporters/evehicles_to_gcs.py
+++ 02-mage/electric_vehicles/data_exporters/evehicles_to_gcs.py
@@ -5,7 +5,7 @@ import os
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS']='/home/src/credentials/keys.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/src/credentials/keys.json'
 
 bucket_name = 'electric_vehicles_dtc-de-course-412223'
 project_id = 'dtc-de-course-412223'
@@ -13,20 +13,8 @@ table_name = 'electric_vehicles'
 
 root_path = f'{bucket_name}/{table_name}'
 
+
 @data_exporter
-# def export_data(data, *args, **kwargs):
-
-#     table = pa.Table.from_pandas(data)
-
-#     gcs = pa.fs.GcsFileSystem()
-
-#     pq.write_to_dataset(
-#         table,
-#         root_path=root_path,
-#         partition_cols=['transaction_year'],
-#         filesystem=gcs
-#     )
-
 def export_data(data, *args, **kwargs):
 
     table = pa.Table.from_pandas(data)
